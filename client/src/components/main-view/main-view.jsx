352bba05efd1adb2507b87b16dfb45f69d1c6bc7
seanjkr <seankoppreddy@gmail.com>
2020-10-04 16:00:11 -0500

    Added main-view, movie-view and movie-card components. Created functions to navigate through them.

---
+++ client/src/components/main-view/main-view.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import axios from 'axios';
+
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+
+export class MainView extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            movies : null,
+            selectedMovie : null
+        };
+    }
+      
+    componentDidMount() {
+        axios.get('https://seans-movie-api.herokuapp.com/movies')
+          .then(response => {
+            this.setState({
+              movies: response.data
+            });
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+
+    onMovieClick( movie ) {
+        this.setState( { 
+            selectedMovie : movie
+        });
+    }
+    
+
+    render() {
+        const { movies, selectedMovie } = this.state;
+    
+        if ( !movies ) return <div className="main-view"/>;
+    
+        return (
+            <div className = "main-view">
+                {selectedMovie
+                    ? <MovieView movie = { selectedMovie } back = { movie => this.onMovieClick( !movie )} />
+                    : movies.map( movie => (
+                        <MovieCard key = { movie._id } movie = { movie } onClick = { movie => this.onMovieClick( movie )}/>
+                    ))
+                }
+            </div>    
+        );
+    }
+}
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {MainView} from './components/main-view/main-view.jsx';
 import './index.scss';
 
 class MyMovieApp extends React.Component {
   render() {
-    return (
-      <div className="movie-app">
-        <div>Good Morning</div>
-      </div>
-    );
+    return <MainView/>;
   }
 }
 
-const container = document.getElementByClassName( 'app-container' )[0];
+const container = document.getElementsByClassName('app-container')[0];
 
-ReactDOM.render( React.createElement( MyMovieApp ), container );
+ReactDOM.render(React.createElement(MyMovieApp), container);
